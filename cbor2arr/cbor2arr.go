cfe42adca0f4ec462c54047dcc96aff8db41609a
yanagitani <takanori.yanagitani@gmail.com>
2024-10-14 06:52:36 +0900

    docs: more docs

--- cbor2arr/cbor2arr.go
+++ cbor2arr/cbor2arr.go
@@ -1,3 +1,7 @@
 package cbor2map
 
+// Parses a CBOR array and store the result to the buffer.
+//
+// # Arguments
+//   - (the buffer) *[]any: The buffer to store the parse result.
 type CborToArray func(*[]any) error
